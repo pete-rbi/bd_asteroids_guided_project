0316f16be9c7dcc9bad58172d5edb0e518bb8772
pete_rbi <pmacbraun@gmail.com>
2026-09-01 17:42:47 -0400

    CH03 L04 removed return from draw method

--- player.py
+++ player.py
@@ -5,6 +5,8 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS, LINE_WIDTH
 # CH03 L03
 from constants import PLAYER_TURN_SPEED
+# CH04 LO4
+from constants import PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x:float, y:float):
@@ -21,7 +23,8 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen: pygame.Surface):
-        return pygame.draw.polygon(screen,"white",self.triangle(), LINE_WIDTH)
+        # CH03 L04 removed 'return' from begining of bottom line , now jq == True
+        pygame.draw.polygon(screen,"white",self.triangle(), LINE_WIDTH)
 
     # CH03 L03
     def rotate(self, dt):
@@ -35,3 +38,18 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        # CH03 L04
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    # CH03 L04
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+        
+
